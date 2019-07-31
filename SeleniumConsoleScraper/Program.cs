3c4799e7816abb3bb2949f758adbd89181245052
Jim Doyle <indellibleink@gmail.com>
2019-07-31 10:44:48 -0400

    added datestamp/avgvol3m/method fields

--- SeleniumConsoleScraper/Program.cs
+++ SeleniumConsoleScraper/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
-namespace testConSel
+namespace SeleniumConsoleScraper
 {
     public class Program
     {
@@ -16,163 +16,133 @@ namespace testConSel
             var runScraper = new Scraper();
 
             runScraper.Scrape();
+           
         }
-    }
-
-    public class Stock
-    {
-        public string Symbol { get; set; }
-        public string Price { get; set; }
-        public string Change { get; set; }
-        public string PChange { get; set; }
-        public string Volume { get; set; }
-        public string MarketCap { get; set; }
-        public string ScrapeTime { get; set; }
-    }
-
-    public class Scraper
-    {
-        public List<Stock> Scrape()
+        public class Stock
         {
-            //ChromeOptions options = new ChromeOptions();
+            public string DateStamp { get; set; }
+            public string Symbol { get; set; }
+            public string LastPrice { get; set; }
+            public string Change { get; set; }
+            public string ChgPc { get; set; }
+            public string MarketTime { get; set; }
+            public string Volume { get; set; }
+            public string AvgVol3m { get; set; }
+            public string MarketCap { get; set; }
+            public string Method { get; set; }
 
-            //// Add capbilities to ChromeOptions
-            //options.AddArguments("test -Type", "--ignore-certificate-errors", "--disable-gpu", "disable-popups");
-
-            //// Launching browser with desired capabilities + proper binary file location
-            ////IWebDriver driver = new ChromeDriver(@"\Users\gregs\Desktop\CD\CapstoneConsoleApp\CapstoneConsoleApp\bin", options);
-            //IWebDriver driver = new ChromeDriver();
-            //driver.Manage().Window.Maximize();
-
-            //// create default wait 100 seconds + set to ignore the most persistent and disruptive timeout errors that break scraper
-            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(100000));
-            //wait.IgnoreExceptionTypes(typeof(NoSuchElementException), typeof(WebDriverTimeoutException), typeof(UnhandledAlertException));
-
-            //driver.Navigate().GoToUrl("https://login.yahoo.com/config/login?.src=finance&amp;.intl=us&amp;.done=https%3A%2F%2Ffinance.yahoo.com%2Fportfolios");
-
-            //wait.Until(waiter => waiter.FindElement(By.Id("login-username")));
-            //IWebElement username = driver.FindElement(By.Id("login-username"));
-            //username.SendKeys("jfdoyle_iii");
-            //username.SendKeys(Keys.Enter);
-
-            //// EXPLICIT WAIT
-            //wait.Until(waiter => waiter.FindElement(By.Id("login-passwd")));
-            //IWebElement password = driver.FindElement(By.Id("login-passwd"));
-            //password.SendKeys("m93Fe8YHn");
-            //IWebElement loginButton = driver.FindElement(By.Id("login-signin"));
-            //loginButton.SendKeys(Keys.Enter);
-            ChromeOptions option = new ChromeOptions();
-            option.AddArgument("--headless");
-            option.AddArgument("window-size=1200,1100");
-
-            using (IWebDriver driver = new ChromeDriver(option))
+        }
+      
+        public class Scraper
+        {
+            public List<Stock> Scrape()
             {
-                driver.Navigate().GoToUrl("https://finance.yahoo.com");
 
-                WebDriverWait waitSignIn = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                waitSignIn.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("uh-signedin")));
+                ChromeOptions option = new ChromeOptions();
+                option.AddArgument("--headless");
+                option.AddArgument("window-size=1200,1100");
 
-                IWebElement signIn = driver.FindElement(By.Id("uh-signedin"));
-                signIn.Click();
-
-                WebDriverWait waitLogin = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                waitLogin.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-username")));
-
-                IWebElement LoginField = driver.FindElement(By.Id("login-username"));
-                LoginField.SendKeys("jfdoyle_iii");
-                LoginField.SendKeys(Keys.Enter);
-
-                WebDriverWait waitPassword = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                waitPassword.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
-
-
-                IWebElement passwordField = driver.FindElement(By.Id("login-passwd"));
-                passwordField.SendKeys("m93Fe8YHn");
-                passwordField.SendKeys(Keys.Enter);
-
-
-
-                driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_2/view/v1");
-                WebDriverWait waitStockTable = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                waitStockTable.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//table")));
-
-
-                IWebElement list = driver.FindElement(By.TagName("tbody"));
-                ReadOnlyCollection<IWebElement> items = list.FindElements(By.TagName("tr"));
-                int count = items.Count;
-
-
-
-                // create list to store stock data type
-                List<Stock> stockList = new List<Stock>();
-
-                // print the contents of that list to the console
-                Console.WriteLine("The Portfolio of stocks is as follows: ");
-
-                //Loop iterate through portfolio of stocks, gathering data
-                // current issue is the 
-                for (int i = 1; i <= count; i++)
+                using (IWebDriver driver = new ChromeDriver(option))
                 {
-                    string symbol = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[1]/a")).GetAttribute("innerText");
-                    Console.WriteLine(symbol);
-                    string price = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[2]/span")).GetAttribute("innerText");
-                    Console.WriteLine(price);
-                    string change = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[3]/span")).GetAttribute("innerText");
-                    Console.WriteLine(change);
-                    string pchange = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[4]/span")).GetAttribute("innerText");
-                    Console.WriteLine(pchange);
-                    string volume = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[7]/span")).GetAttribute("innerText");
-                    Console.WriteLine(volume);
-                    string marketcap = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[13]/span")).GetAttribute("innerText");
-                    Console.WriteLine(marketcap);
-                    string scrapeTime = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[6]/span")).GetAttribute("innerText");
-                    Console.WriteLine(scrapeTime);
+                    driver.Navigate().GoToUrl("https://finance.yahoo.com");
+
+                    WebDriverWait waitSignIn = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    waitSignIn.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("uh-signedin")));
+
+                    IWebElement signIn = driver.FindElement(By.Id("uh-signedin"));
+                    signIn.Click();
+
+                    WebDriverWait waitLogin = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    waitLogin.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-username")));
+
+                    IWebElement LoginField = driver.FindElement(By.Id("login-username"));
+                    LoginField.SendKeys("jfdoyle_iii");
+                    LoginField.SendKeys(Keys.Enter);
+
+                    WebDriverWait waitPassword = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    waitPassword.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
 
 
-                    // for each stock entry, a new stock object is created
-                    // the above data is set equal to the field within the object
-                    Stock newStock = new Stock();
-                    newStock.Symbol = symbol;
-                    newStock.Price = price;
-                    newStock.Change = change;
-                    newStock.PChange = pchange;
-                    newStock.ScrapeTime = scrapeTime;
-                    newStock.Volume = volume;
-                    newStock.MarketCap = marketcap;
+                    IWebElement passwordField = driver.FindElement(By.Id("login-passwd"));
+                    passwordField.SendKeys("m93Fe8YHn");
+                    passwordField.SendKeys(Keys.Enter);
 
-                    // that stock is then added to the list of stocks
-                    stockList.Add(newStock);
+
+
+                    driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_2/view/v1");
+                    WebDriverWait waitStockTable = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                    waitStockTable.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//table")));
+
+                   // //*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[1]/td[13]/span
+
+                    IWebElement list = driver.FindElement(By.TagName("tbody"));
+                    ReadOnlyCollection<IWebElement> items = list.FindElements(By.TagName("tr"));
+                    int count = items.Count;
+                    Console.WriteLine();
+                    Console.WriteLine("items: {0}",count);
+
+
+
+                    // create list to store stock data type
+                    List<Stock> stockList = new List<Stock>();
+
+                    // print the contents of that list to the console
+                    Console.WriteLine("The Portfolio of stocks is as follows: ");
+
+                    //Loop iterate through portfolio of stocks, gathering data
+                    // current issue is the 
+                    for (int i = 1; i <= count; i++)
+                    {
+                        Console.WriteLine(DateTime.Now);
+                        string symbol = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[1]/a")).GetAttribute("innerText");
+                        Console.WriteLine(symbol);
+                        string lastPrice = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[2]")).GetAttribute("innerText");
+                        Console.WriteLine(lastPrice);
+                        string change = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[3]")).GetAttribute("innerText");
+                        Console.WriteLine(change);
+                        string chgpc = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[4]")).GetAttribute("innerText");
+                        Console.WriteLine(chgpc);
+                        string marketTime = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[6]")).GetAttribute("innerText");
+                        Console.WriteLine(marketTime);
+                        string volume = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[7]")).GetAttribute("innerText");
+                        Console.WriteLine(volume);
+                        string avgvol3m = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[9]")).GetAttribute("innerText");
+                        Console.WriteLine(avgvol3m);
+                        string marketcap = driver.FindElement(By.XPath("//*[@id=\"pf-detail-table\"]/div[1]/table/tbody/tr[" + i + "]/td[13]")).GetAttribute("innerText");
+                        Console.WriteLine(marketcap);
+                        string method = "Sel";
+                        Console.WriteLine(method);
+                        
+                        // for each stock entry, a new stock object is created
+                        // the above data is set equal to the field within the object
+                        Stock newStock = new Stock();
+                        newStock.DateStamp = DateTime.Now.ToString();
+                        newStock.Symbol = symbol;
+                        newStock.LastPrice = lastPrice;
+                        newStock.Change = change;
+                        newStock.ChgPc = chgpc;
+                        newStock.MarketTime = marketTime;
+                        newStock.Volume = volume;
+                        newStock.AvgVol3m = avgvol3m;
+                        newStock.MarketCap = marketcap;
+                        newStock.Method = method;
+
+                        // that stock is then added to the list of stocks
+                        stockList.Add(newStock);
+                    }
+
+                    driver.Quit();
+
+                    return stockList;
+
+
                 }
-
-                driver.Quit();
-
-                return stockList;
-
-                //using (SelStocksContext db = new SelStocksContext())
-                //{
-
-                //    for (int i = 0; i < stockList.Count; i++)
-                //    {
-
-                //        testSel stockTable = new testSel
-                //        {
-
-                //            Symbol = stockList[i].ToString(),
-                //            LastPrice = stockList[i].ToString(),
-                //            Change = stockList[i].ToString(),
-                //            ChgPc = stockList[i].ToString(),
-                //            Currency = stockList[i].ToString(),
-                //            MarketTime = stockList[i].ToString(),
-                //            Volume = stockList[i].ToString(),
-                //            AvgVol3m = stockList[i].ToString(),
-                //            MarketCap = stockList[i][1.ToString(),
-                //        };
-
-                //        db.StockTables.Add(stockTable);
-                //        db.SaveChanges();
-                //    }
-                //}
             }
         }
     }
 }
+
+
+
+
+
---
+++ SeleniumConsoleScraper/ToDataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumConsoleScraper
+{
+    class ToDataBase
+    {
+        //using (SelStocksContext db = new SelStocksContext())
+        //{
+
+        //    for (int i = 0; i < stockList.Count; i++)
+        //    {
+
+        //        testSel stockTable = new testSel
+        //        {
+
+        //            Symbol = stockList[i].ToString(),
+        //            LastPrice = stockList[i].ToString(),
+        //            Change = stockList[i].ToString(),
+        //            ChgPc = stockList[i].ToString(),
+        //            Currency = stockList[i].ToString(),
+        //            MarketTime = stockList[i].ToString(),
+        //            Volume = stockList[i].ToString(),
+        //            AvgVol3m = stockList[i].ToString(),
+        //            MarketCap = stockList[i][1.ToString(),
+        //        };
+
+        //        db.StockTables.Add(stockTable);
+        //        db.SaveChanges();
+        //    }
+        //}
+    }
+}
